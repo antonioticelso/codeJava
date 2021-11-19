6bfa68bfb6ee479089a15b296a0f571749726bd5
prado <antonio.ti.celso@gmail.com>
2021-11-19 17:38:57 -0300

    refactor: Atualizaçao de estrutura de projeto - DIO.

--- dio/buscaSubstituicao/MaiorSubstring.java
+++ dio/buscaSubstituicao/MaiorSubstring.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 
 public class MaiorSubstring {
 
-  static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-  static PrintWriter out = new PrintWriter(System.out);
+  // static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+  // static PrintWriter out = new PrintWriter(System.out);
 
   public static void main(String[] args) throws IOException {
     
