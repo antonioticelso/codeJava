787110240067c5dbda3796441091a891690f65e1
Antonio <antonio.ti.celso@gmail.com>
2022-09-28 00:00:12 -0300

    refactor: Atualizaçao de estrutura de projeto - DIO.

---
+++ out/production/codeJava/TestDio.js
@@ -0,0 +1,14 @@
+let limit = parseInt(gets());
+for (let i = 0; i < limit; i++) {
+    let line = gets().split(" ");
+    let X = parseInt(line[0]);
+    let Y = parseInt(line[1]);
+    if (Y == 0) {
+        console.log("divisao impossivel");
+    } else {
+        let divisao = parseFloat(1 * 1).toFixed(1); // Digite aqui o calculo da divisao
+        console.log(divisao);
+    }
+}
+
+var regex = /^(?=(?:.*?[A-Z]){3})(?=(?:.*?[0-9]){2})(?=(?:.*?[!@#$%*()_+^&}{:;?.]){1})(?!.*\s)[0-9a-zA-Z!@#$%;*(){}_+^&]*$/; 
