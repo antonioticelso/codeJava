0cdf91c2df8c369622ab7ac331fe63c8c1e846da
Antonio <antonio.ti.celso@gmail.com>
2022-10-11 20:42:36 -0300

    quebecBasic -> Novas classes de desafios - DIO.

--- dio/quebecBasico/PreenchimentoDeVetor.java
+++ dio/quebecBasico/PreenchimentoDeVetor.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class PreenchimentoDeVetor {
 
     public static void main(String[] args){
-//        Scanner leitor = new Scanner(System.in);
-//        int t = leitor.nextInt();
-        int t = 3;
+        Scanner leitor = new Scanner(System.in);
+        int t = leitor.nextInt();
+//        int t = 3;
 
         for(int i = 0; i < 1000; i++ ){
             for(int j = 0; j< t; j++){
