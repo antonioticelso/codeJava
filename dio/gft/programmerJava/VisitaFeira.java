384033de363de0de26f56025cb94fb089058fce4
Antonio <antonio.ti.celso@gmail.com>
2022-11-03 09:45:45 -0300

    cloudFullStack -> Novas classes de desafios.

---
+++ dio/gft/programmerJava/VisitaFeira.java
@@ -0,0 +1,19 @@
+package gft.programmerJava;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class VisitaFeira {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int total = a + b;
+
+        System.out.println("X = " + total);
+    }
+
+}
